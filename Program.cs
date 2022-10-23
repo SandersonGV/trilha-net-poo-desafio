a6e06b58d1a3e01c401c24ea1f380ff74e2299b9
Sanderson <virgolinos@gmail.com>
2022-10-23 10:28:03 -0300

    completando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("21988882222","Modelo-A","00123212321",500000);
+nokia.Ligar();
+nokia.InstalarAplicativo("youtube");
+
+Console.WriteLine(" ");
+
+Iphone iphone = new Iphone("2195585632","Modelo-B","11154654862",100000);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("instagram");
